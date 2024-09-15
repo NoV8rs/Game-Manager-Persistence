be98ffdbf21d789eb78744c77a11bf26f088e01c
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2024-09-14 23:01:06 -0300

    Player Scripts
    Player movement script is being set up, with Camera Clamps, Max Speed, and Jumping is in this too.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* Character Controller - Using CharacterBody in Unity
+     * Movement - Using Rigidbody in Unity, Forwards, Backwards, Left, Right, max speed, and sprinting
+     * Jumping - Using Rigidbody in Unity, Jumping with a force, max jump height, and a check for if the player is grounded
+     * Camera - Using Camera in Unity, Rotating the camera with the mouse, and clamping the angle of the camera
+     */
+    
+    private CharacterController characterController;
+    
+    //Speed
+    public float playerSpeed = 10.0f;
+    public float sprintSpeed = 15.0f;
+    public float rotationSpeed = 100.0f;
+    
+    //Jumping
+    float jumpForce = 8.0f;
+    float maxJumpHeight = 1.0f;
+    float gravity = 20.0f;
+    bool isGrounded;
+    
+    //Camera
+    public Transform playerCamera;
+    public float cameraRotationLimit = 80.0f;
+    
+
+    public void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
+    {
+        // Movement
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(move * Time.deltaTime * playerSpeed);
+        
+    }
+
+
+
+
+}
